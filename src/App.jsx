f83fb08dcb873d1c20b0eac6a004bf67f489de2b
rsinno45 <rsinno45@gmail.com>
2025-01-01 17:40:06 -0600

    updated build bug

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import HomePage from "./pages/homePage";
+import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
 import MembershipPage from "./pages/MembershipPage";
 import EventsPage from "./pages/EventsPage";
