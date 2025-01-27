0a87dea20ad7f28b9603a39705cef695ac0d8e29
rsinno45 <rsinno45@gmail.com>
2025-01-26 18:03:01 -0600

    updated file strcuture

--- src/pages/ContractingNeeds.jsx
+++ src/pages/ContractingNeeds.jsx
@@ -171,6 +171,7 @@ const ContractingNeeds = () => {
                       <img
                         src={resource.imageSrc}
                         alt={resource.alt}
+                        loading="lazy"
                         className="w-full h-48 object-cover transition-transform duration-300 group-hover:scale-105"
                       />
                       <div className="absolute inset-0 bg-gradient-to-t from-black/60 via-black/20 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300" />
